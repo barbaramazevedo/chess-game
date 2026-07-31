421f5fcf0f9e036735b99d17c9ee68e5aa289557
Barbara Azevedo <barbara.azevedo@sap.com>
2026-07-31 17:09:38 -0300

    feat: start to implement board and piece

--- backend/src/application/App.java
+++ backend/src/application/App.java
@@ -1,9 +1,9 @@
+import boardgame.Board;
 import boardgame.Position;
 
 public class App {
     public static void main(String[] args) throws Exception {
        
-        Position pos = new Position(3, 5);
-        System.out.print(pos);
+        Board board= new Board(8, 8);
     }
 }
